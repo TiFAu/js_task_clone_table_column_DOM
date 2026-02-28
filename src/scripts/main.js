cb1ba207c6e8cee2f4dbe8baec8453e6e5fb824a
TiFAu <turchin.anton2@gmail.com>
2026-02-28 12:40:59 +0200

    added first solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const allTr = [...document.querySelectorAll('tr')];
+
+allTr.forEach((element) => {
+  const allThAndTD = [...element.children];
+  const insertableElement = allThAndTD
+    .find((elem) => elem.previousElementSibling === null)
+    .nextElementSibling.cloneNode(true);
+  const lastElement = allThAndTD.find(
+    (elem) => elem.nextElementSibling === null,
+  );
+
+  lastElement.insertAdjacentElement('beforebegin', insertableElement);
+});
